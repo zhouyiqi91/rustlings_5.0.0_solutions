95540ef06f7a75f290006af62e9cdd9817e90600
zhouyiqi91 <386346852@qq.com>
2022-08-18 15:15:33 +0800

    options

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -1,7 +1,6 @@
 // options1.rs
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 // you can modify anything EXCEPT for this function's signature
 fn print_number(maybe_number: Option<u16>) {
@@ -14,7 +13,11 @@ fn print_number(maybe_number: Option<u16>) {
 // TODO: Return an Option!
 fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22
-    ???
+    if time_of_day <= 10 {
+        return Some(5);
+    } else {
+        return None;
+    }
 }
 
 #[cfg(test)]
@@ -32,6 +35,6 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get at the value contained in the Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams, None);
     }
 }
